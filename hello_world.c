9e3abbe6847dde67d79ece64f462cc1356adf59e
Chaitanya Jaiswal <21bch073@nith.ac.in>
2023-04-02 17:48:32 +0530

    Undone the commit but retained changes

--- hello_world.c
+++ hello_world.c
@@ -1,3 +1,9 @@
-main(){
+#include <stdio.h>
+
+int main(void){
 	printf("hello, world\n");
+
+	return 0;
 }
+
+//here we've removed the latest commit but have retained the changes in the staging area
