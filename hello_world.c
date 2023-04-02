0a1abe45534b87a2aede354e89c430f8e8c31b60
Chaitanya Jaiswal <21bch073@nith.ac.in>
2023-04-02 23:47:16 +0530

    Undone the commit but retained changes in the staging area

--- hello_world.c
+++ hello_world.c
@@ -1,3 +1,10 @@
-main(){
+#include <stdio.h>
+
+int main(void){
 	printf("hello, world\n");
+
+	return 0;
 }
+
+//here we've removed the latest commit but have retained the changes in the staging area
+//in the previous branch we've removed the commit and retianed changes in the working directory but this time file is in the staging area
